8e7d4ec96d2be3c8cc712dc3141bc96bfb3a885a
KanataOsako <kanata-osako@outlook.jp>
2024-06-19 15:28:23 +0900

    Add files via upload

---
+++ app.py
@@ -0,0 +1,25 @@
+import flask
+from flask import request, Response
+import os
+import json
+
+app = flask.Flask(__name__)
+
+@app.route('/', methods=["GET","POST"])
+def flask_app():
+    data = request.data.decode('utf-8')
+    data = json.loads(data)
+    # for challenge of slack api
+    if 'challenge' in data:
+        token = str(data['challenge'])
+        return Response(token, mimetype='text/plane')
+    # for events which you added
+    if 'event' in data:
+        print("get event")
+        event = data['event']
+        if 'user' in event:
+            print("user = ", event["user"])
+        if "text" in event:
+            print("text = ", event["text"])
+    return Response("nothing", mimetype='text/plane')
+
